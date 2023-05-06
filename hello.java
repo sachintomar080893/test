d4e82916a85ec4c8ba00ddf81e2b575dfdb1fe2d
sachintomar08 <58623454+sachintomar08@users.noreply.github.com>
2023-05-06 13:33:07 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,6 +3,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-         System.out.println("Hello, Sachin!"); 
+        System.out.println("Hello, Sachin!"); 
+        System.out.println("Hello, Tomar ji!"); 
     }
 }
